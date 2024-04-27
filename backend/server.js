77ef690986868d0e76a883d70becfb8e0e32b0df
MHQA-138171 <omegameen1@gmail.com>
2024-04-28 01:47:12 +0500

    changing port

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
 })
 app.get('/weather', getWeatherByCity)
 
-app.listen(3000, () => {
-    console.log("listening on 3000");
+app.listen(8000, () => {
+    console.log("listening on 8000");
 })
